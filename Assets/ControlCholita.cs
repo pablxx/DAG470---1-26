0cdefa2d5744c263e4decd4685010e0e723f8f72
PabloSR <phax.rod@gmail.com>
2026-05-08 15:46:23 -0400

    Cholito IK
    Agregado el personaje cholito con IK y animacion basica

--- Assets/ControlCholita.cs
+++ Assets/ControlCholita.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class ControlCholita : MonoBehaviour
@@ -11,6 +12,8 @@ public class ControlCholita : MonoBehaviour
     public Animator anim;
 
     public float distanciaPiso = 0.25f;
+
+    bool caminando = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,14 +22,39 @@ public class ControlCholita : MonoBehaviour
 
     private void Update()
     {
+        float entradaX = Input.GetAxis("Horizontal");
+
+        if (entradaX != 0) {
+            caminando = true;
+            Debug.Log(entradaX);
+            rb.linearVelocityX = entradaX * velMovimiento;
+        }
+        else
+        {
+            caminando = false;
+        }
+
+
         if (Input.GetButtonDown("Jump"))
         {
             rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
         }
 
+        anim.SetBool("caminando", caminando);
         anim.SetFloat("velY", rb.linearVelocityY);
+
+        if (entradaX > 0 && transform.localScale.x < 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
+        else if (entradaX < 0 && transform.localScale.x > 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
+
     }
 
+   
     private void FixedUpdate()
     {
         DetectarPiso();
